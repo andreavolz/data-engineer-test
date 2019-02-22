50c1de834fbd4738a6e8deda5678845a3a776ba1
andreavolz <andreavolzg@gmail.com>
2019-02-22 19:47:21 -0300

    Dados para conexão corrigidos

--- conn.php
+++ conn.php
@@ -1,16 +1,16 @@
 <?php
 	$servername = "localhost";
 	$dbname = "indeorum";
-	//$username = "username";
-	//$password = "password";
+	$username = "root";
+	$password = "12345";
 
 	// Create connection
-	$conn = new mysqli($servername,$dbname);
+	$conn = new mysqli($servername,$username,$password,$dbname);
 
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	} 
-	echo "Connected successfully";
+
 ?>
 
